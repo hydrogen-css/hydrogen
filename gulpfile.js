11dc7746cbae69b9ffc05e9819956ed0a7298bd9
Josh Beveridge <josh.beveridge@outlook.com>
2021-03-18 18:30:33 -0400

    Adds support for the shadow utility, adds config support for both specifying a markup folder and a styles folder.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const { series, parallel, src, dest, watch } = require('gulp');
 const del = require('del');
 var footer = require('gulp-footer');
 const fs = require('fs');
+const concat = require('gulp-concat');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
@@ -11,6 +12,7 @@ const replace = require('gulp-replace');
 const sass = require('gulp-sass');
 var source = require('vinyl-source-stream');
 const { conflicts } = require('yargs');
+const { type } = require('os');
 
 // Set the Sass compiler to Dart Sass.
 sass.compiler = require('sass');
@@ -20,121 +22,141 @@ var nodePackage = JSON.parse(fs.readFileSync('./package.json'));
 // Get the package's current version number.
 const version = nodePackage.version;
 
+// Import the default JSON config file.
+var defaults = JSON.parse(fs.readFileSync('src/h2.default.json'));
+
 // Import the user's JSON config file.
-var config = JSON.parse(fs.readFileSync("./src/stage/h2.config.json"));
+var config = JSON.parse(fs.readFileSync('./src/stage/h2.config.json'));
 
 // Cached H2
 // This is required because each config setting needs to update a single instance of the Hydrogen file, otherwise they each create their own and overwrite the last.
 function cacheH2() {
   return src('src/styles/hydrogen.scss')
-  .pipe(dest('src/stage/cache'));
+  .pipe(dest(config.styles.path + '/hydrogen'));
 }
 
 // Media
-var media = false;
 var mediaConfig = "";
 
-if (config.media != null) {
-  media = true;
+if (config.media != null && config.media != undefined && config.media.length > 0) {
   var mediaStringStart = '$h2-map-media: ("b": "screen",';
   var mediaStringContent = '';
   var mediaStringEnd = ');';
-  for (let obj in config.media) {
-    var mediaString = '"' + obj + '": ' + '"screen and (min-width: ' + config.media[obj] + ')",';
+  config.media.forEach(function(mediaQuery) {
+      // console.log(mediaQuery);
+    var mediaString = '"' + mediaQuery.key + '": ' + '"screen and (min-width: ' + mediaQuery.value + ')",';
     mediaStringContent = mediaStringContent.concat(mediaString);
-  }
+      // console.log(mediaStringContent);
+  });
+  mediaConfig = mediaConfig.concat(mediaStringStart).concat(mediaStringContent).concat(mediaStringEnd);
+} else {
+  var mediaStringStart = '$h2-map-media: ("b": "screen",';
+  var mediaStringContent = '';
+  var mediaStringEnd = ');';
+  defaults.media.forEach(function(mediaQuery) {
+      // console.log(mediaQuery);
+    var mediaString = '"' + mediaQuery.key + '": ' + '"screen and (min-width: ' + mediaQuery.value + ')",';
+    mediaStringContent = mediaStringContent.concat(mediaString);
+      // console.log(mediaStringContent);
+  });
   mediaConfig = mediaConfig.concat(mediaStringStart).concat(mediaStringContent).concat(mediaStringEnd);
 }
 
 function customMedia() {
-  if (media == true) {
-    return src('src/styles/maps/_map-media-user.scss')
-    .pipe(footer(mediaConfig))
-    .pipe(dest('src/stage/styles/maps'));
-  } else {
-    return src('src/styles/maps/_map-media-default.scss')
-    .pipe(dest('src/stage/styles/maps'))
-  }
-}
-
-function userMedia() {
-  if (media == true) {
-    return src('src/stage/cache/hydrogen.scss')
-    .pipe(replace('map-media-default', 'map-media-user'))
-    .pipe(dest('src/stage/cache'));
-  } else {
-    return src('src/stage/cache/hydrogen.scss')
-    .pipe(dest('src/stage/cache'));
-  }
+  return src('src/styles/maps/_map-media.scss')
+  .pipe(footer(mediaConfig))
+  .pipe(dest(config.styles.path + '/hydrogen/maps'));
 }
 
 // Color
-var color = false;
 var colorConfig = "";
 
-if (config.colors != null) {
-  color = true;
+if (config.colors != null && config.colors != undefined && config.colors.length > 0) {
   var colorStringStart = '$h2-map-color: (';
   var colorStringContent = '';
   var colorStringEnd = ');';
-  for (let obj in config.colors) {
-    var colorString = '"' + obj + '": ' + config.colors[obj] + ',';
+  config.colors.forEach(function(color) {
+    var colorString = '"' + color.key + '": ' + color.value + ',';
     colorStringContent = colorStringContent.concat(colorString);
-  }
+  });
+  colorConfig = colorConfig.concat(colorStringStart).concat(colorStringContent).concat(colorStringEnd);
+} else {
+  var colorStringStart = '$h2-map-color: (';
+  var colorStringContent = '';
+  var colorStringEnd = ');';
+  defaults.colors.forEach(function(color) {
+    var colorString = '"' + color.key + '": ' + color.value + ',';
+    colorStringContent = colorStringContent.concat(colorString);
+  });
   colorConfig = colorConfig.concat(colorStringStart).concat(colorStringContent).concat(colorStringEnd);
 }
 
 function customColor() {
-  if (color == true) {
-    return src('src/styles/maps/_map-color-user.scss')
-    .pipe(footer(colorConfig))
-    .pipe(dest('src/stage/styles/maps'));
-  } else {
-    return src('src/styles/maps/_map-color-default.scss')
-    .pipe(dest('src/stage/styles/maps'))
-  }
+  return src('src/styles/maps/_map-color.scss')
+  .pipe(footer(colorConfig))
+  .pipe(dest(config.styles.path + '/hydrogen/maps'));
 }
 
-function userColor() {
-  if (color == true) {
-    return src('src/stage/cache/hydrogen.scss')
-    .pipe(replace('map-color-default', 'map-color-user'))
-    .pipe(dest('src/stage/cache'));
-  } else {
-    return src('src/stage/cache/hydrogen.scss')
-    .pipe(dest('src/stage/cache'));
-  }
+// Shadow
+var shadowConfig = "";
+
+if (config.shadows != null && config.shadows != undefined && config.shadows.length > 0) {
+  var shadowStringStart = '$h2-map-shadow: (';
+  var shadowStringContent = '';
+  var shadowStringEnd = ');';
+  config.shadows.forEach(function(shadow) {
+    var shadowString = '"' + shadow.key + '": "' + shadow.value + '",';
+    shadowStringContent = shadowStringContent.concat(shadowString);
+  });
+  shadowConfig = shadowConfig.concat(shadowStringStart).concat(shadowStringContent).concat(shadowStringEnd);
+} else {
+  var shadowStringStart = '$h2-map-shadow: (';
+  var shadowStringContent = '';
+  var shadowStringEnd = ');';
+  defaults.shadows.forEach(function(shadow) {
+    var shadowString = '"' + shadow.key + '": "' + shadow.value + '",';
+    shadowStringContent = shadowStringContent.concat(shadowString);
+  });
+  shadowConfig = shadowConfig.concat(shadowStringStart).concat(shadowStringContent).concat(shadowStringEnd);
 }
 
-// Move Cached H2
-// This moves the configured Hydrogen file to the stage to be compiled.
-function moveCachedH2() {
-  return src('src/stage/cache/hydrogen.scss')
-  .pipe(dest('src/stage/styles'))
+function customShadow() {
+  return src('src/styles/maps/_map-shadow.scss')
+  .pipe(footer(shadowConfig))
+  .pipe(dest(config.styles.path + '/hydrogen/maps'));
 }
 
 // Compile
 function compile() {
-  return src('src/stage/styles/hydrogen.scss')
+  return src(config.styles.path + '/hydrogen/hydrogen.scss')
   .pipe(sass())
-  .pipe(dest('src/stage/styles/compiled'));
+  .pipe(dest(config.styles.path + '/hydrogen/compiled'));
 }
 
 // Compress
 function preCleanCompress() {
-  return src('src/stage/styles/compiled/hydrogen.css')
+  return src(config.styles.path + '/hydrogen/compiled/hydrogen.css')
   .pipe(postcss([cssnano({
     preset: ['default', {
       // preset options here, e.g...
       mergeRules: false
   }]
   })]))
-  .pipe(dest('src/stage/styles/compressed'));
+  .pipe(dest(config.styles.path + '/hydrogen/compressed'));
+}
+
+// Get Markup
+function getUserMarkup() {
+  return src(config.markup.path + '/**/*.' + config.markup.type)
+  .pipe(concat('markup.' + config.markup.type))
+  // This destination will have to be the CSS folder the user specifies.
+  .pipe(dest(config.styles.path + '/hydrogen/markup'))
 }
 
 // Remove Unwanted CSS
 function cleanCSS(done) {
-  var sampleHTML = '<div data-h2-bg-color="b(green) s(red)"> <div data-h2-bg-color="l(green[.3])"> <div data-h2-bg-color="s(red[.5])>" <div data-h2-alignment="b(center)"></div> <div       data-h2-bg-color="b(green-light) s(red-dark[.9])">';
+  // Get the Hydrogen markup from the user's folder.
+  var markup = fs.readFileSync(config.styles.path + '/hydrogen/markup/markup.' + config.markup.type).toString();
   // Get all instances of Hydrogen data attributes (data-h2-*="*").
   var dataRegex = /data-h2-([^"]*)="([^"]*)"/g;
   // Get the utility portion of the attribute (data-h2-*).
@@ -143,12 +165,27 @@ function cleanCSS(done) {
   var valueRegex = /.\(([^)]*)\)/g;
   // var valueRegex = /.\(.*\)/g;
   // Get the temporary compressed Hydrogen file, located in the user's specified location in the config.
-  var sampleCSS = fs.readFileSync('src/stage/styles/compressed/hydrogen.css').toString();
-    // console.log('sampleCSS file: ', sampleCSS);
+  var hydrogenCSS = fs.readFileSync(config.styles.path + '/hydrogen/compressed/hydrogen.css').toString();
+    // console.log('sampleCSS file: ', hydrogenCSS);
+  // Set up a variable list of arrays for each media query in the config. Thanks Chris Wiseman!
+  let queries = {
+    b: []
+  };
+  if (config.media != null && config.media != undefined && config.media.length > 0) {
+    config.media.forEach(function(mediaQuery) {
+      queries[mediaQuery.key] = [];
+    }); 
+  } else {
+    defaults.media.forEach(function(mediaQuery) {
+      queries[mediaQuery.key] = [];
+    }); 
+  }
+    // console.log(queries);
+  // Set up a string variable for our final CSS file.
   var finalCSS = "";
   // We'll then have to parse through each one and break things apart by media query, and add the * selector...
   // e.g. data-h2-bg-color="b(red) m(yellow)" needs to become [data-h2-bg-color*="b(red)"] and [data-h2-bg-color*="m(yellow)"]
-  var usedAttributes = sampleHTML.match(dataRegex);
+  var usedAttributes = markup.match(dataRegex);
     // console.log(usedAttributes);
   if (usedAttributes != null) {
     usedAttributes.forEach(function(attribute) {
@@ -157,51 +194,56 @@ function cleanCSS(done) {
       var values = attribute.match(valueRegex);
       values.forEach(function(value) {
         // Get the media query set for this particular value.
-        var mediaValue = value.match(/^.*?(?=\()/g);
+        var mediaValue = value.match(/^.*?(?=\()/g); // Returns media value: x
           // console.log("media query: ", mediaValue);
-        // We have to build a RegEx to match the correct media query by checking against the list of available media queries can getting their screen value. Don't forget that "b" will always be an option, which has an empty value because it represents all "screen" queries.
+        // We have to build a RegEx to match the correct media query by checking against the list of available media queries and getting their screen value. Don't forget that "b" will always be an option, which has an empty value because it represents all "screen" queries.
         var queryValue;
-        var defaultQueries;
-        if (media == false) {
-          // The user is using Hydrogen's default media queries.
-          defaultQueries = fs.readFileSync('src/styles/maps/_map-media-default.scss').toString();
-        } else if (media == true) {
-          // The user has defined their own media queries.
-          defaultQueries = mediaConfig;
-        }
+        var defaultQueries = mediaConfig;
         // Construct the query RegEx.
         var queryRegEx = `"` + mediaValue + `": ".*?(?:")`;
         // Create the RegEx.
         var createQueryRegEx = new RegExp(queryRegEx, 'g');
         // Search the default queries for the value.
-        var queryMatch = defaultQueries.match(createQueryRegEx);
+        var queryMatch = defaultQueries.match(createQueryRegEx); // Returns media query: "x": "screen and..."
           // console.log(queryMatch);
         // Isolate the query itself so it can be used as text.
         if (queryMatch != null) {
             // console.log("queryMatch: ", queryMatch[0]);
-          queryValue = queryMatch[0].match(/[^"]([^"]*)[^"\s]/g);
-          // console.log("final media query: ", queryValue);
-          var newRegEx = '\\[data-hydrogen-VARVERSION\\] \\[' + utility + '\\*="' + value.replace(/\(/g, '\\(').replace(/\)/g, '\\)').replace(/\[/g, '\\[').replace(/\]/g, '\\]') + '"\\]{([^}])*}'
+          queryValue = queryMatch[0].match(/[^"]([^"]*)[^"\s]/g); // Returns the media side of the media query: screen and...
+            // console.log("final media query: ", queryValue);
+          var newRegEx = '\\[data-hydrogen=("*VARVERSION"*)\\] \\[' + utility + '\\*="' + value.replace(/\(/g, '\\(').replace(/\)/g, '\\)').replace(/\[/g, '\\[').replace(/\]/g, '\\]') + '"\\]{([^}])*}'
             // console.log(value);
+            // console.log(newRegEx);
           var cssRegex = new RegExp(newRegEx, 'g');
             // console.log('css specific regex: ', cssRegex);
             // console.log('test css regex: ', /\[data-h2-bg-color\*="b\(red\)"\]{([^}])*}/g);
-          var cssMatch = sampleCSS.match(cssRegex);
+          var cssMatch = hydrogenCSS.match(cssRegex); // Returns the full CSS selector: [data-hydrogen=VARVERSION] [data-h2-ATTRIBUTE*="MEDIA(VALUE)"]{CSS}
             // console.log('css match values: ', cssMatch);
           if (cssMatch != null) {
             // Transform the matched CSS to include its media query.
-            var CSSwithMedia = '@media ' + queryValue[0] + '{' + cssMatch + '}';
+            // var CSSwithMedia = '@media ' + queryValue[0] + '{' + cssMatch + '}';
               // console.log(CSSwithMedia);
-            finalCSS = finalCSS.concat(CSSwithMedia);
+            // finalCSS = finalCSS.concat(CSSwithMedia);
+            queries[mediaValue] = queries[mediaValue].concat(cssMatch);
+              // console.log(queries);
           }
         } else {
           console.log('There\'s no matching media query in the media query map for the query "' + mediaValue[0] + '".');
         }
       });
     });
+    // Loop through each media query array now that they're populated with CSS and concatenate them into the final file.
+    for (let query in queries) {
+        // console.log(queries[query]);
+      queries[query].forEach(function(item) {
+          // console.log(item);
+        finalCSS = finalCSS + item;
+      })
+    }
       // console.log('final css: ', finalCSS);
-    fs.mkdirSync('src/stage/styles/cleaned');
-    fs.writeFile('src/stage/styles/cleaned/hydrogen.css', finalCSS, function(err) {
+    // Create the cleaned folder and write the file.
+    fs.mkdirSync(config.styles.path + '/hydrogen/cleaned');
+    fs.writeFile(config.styles.path + '/hydrogen/cleaned/hydrogen.css', finalCSS, function(err) {
       if (err) {
         console.log('Uh oh: ', err);
       }
@@ -210,9 +252,16 @@ function cleanCSS(done) {
   done();
 }
 
-// Clean Stage
-function cleanStage() {
-  return del(['src/stage/**/*', '!src/stage/h2.config.json', '!src/stage/index.html', '!src/stage/styles/cleaned']);
+// Compress
+function postCleanCompress() {
+  return src(config.styles.path + '/hydrogen/cleaned/hydrogen.css')
+  .pipe(postcss([cssnano()]))
+  .pipe(dest(config.styles.path + '/hydrogen/final'));
 }
 
-exports.test = series(cleanStage, cacheH2, customMedia, userMedia, customColor, userColor, moveCachedH2, compile, preCleanCompress, cleanCSS);
+// Clean Stage
+function cleanStage() {
+  return del([config.styles.path + '/hydrogen/**/*']);
+}
+
+exports.test = series(cleanStage, cacheH2, customMedia, customColor, customShadow, compile, preCleanCompress, getUserMarkup, cleanCSS, postCleanCompress);
