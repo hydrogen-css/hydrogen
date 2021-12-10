eef85f2e326bafbceab341232ea3eea532c639ca
Josh Beveridge <josh.beveridge@outlook.com>
2021-12-10 13:14:52 -0500

    Colors and gradients are now treated as the same configuration when it comes to checking for key duplicates. This prevents matching CSS output.

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
-console.log('Hydrogen: welcome to Hydrogen! You can get set up by creating a hydrogen.config.json file at the root of your project and running "npx hydrogen-watch".');
+console.log(
+  'Hydrogen: welcome to Hydrogen! You can get set up by running npx h2-init and following the prompts.'
+);
