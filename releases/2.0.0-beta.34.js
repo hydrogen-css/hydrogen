bcb3e2a8e608e2c2e22394a8f90225a7d006c1ea
Josh Beveridge <git@joshbeveridge.ca>
2023-01-06 23:55:15 -0500

    Updates the release notes

--- releases/2.0.0-beta.34.js
+++ releases/2.0.0-beta.34.js
@@ -22,8 +22,84 @@ module.exports = {
       },
       notes: [],
     },
+    {
+      breaking: false,
+      changes: {
+        en: [
+          'The snippets file has been updated with many common CSS properties for ease-of-use.',
+        ],
+      },
+      notes: [],
+    },
+  ],
+  optimizations: [
+    {
+      breaking: true,
+      changes: {
+        en: [
+          "CSS properties and their syntax have been prioritized over Hydrogen's custom attributes, resulting in a handful of breaking changes that were necessary for future proofing the project.",
+        ],
+      },
+      notes: [
+        'border has been migrated to CSS syntax, meaning that the side option is no longer available and commas are invalid.',
+        'background-color no longer supports configured gradients and should be replaced with background or background-image.',
+        "offset now represents CSS offset and should be replaced with location to retain Hydrogen's original functionality.",
+        'visibility continues to support invisible as a custom value, but the styles associated with visible have been moved to the revealed key in favor of supporting the actual CSS visible value.',
+      ],
+      properties: [
+        'border',
+        'background-color',
+        'offset',
+        'location',
+        'visibility',
+      ],
+    },
+    {
+      breaking: true,
+      changes: {
+        en: [
+          'As a part of processing configured values, color modifiers now have to be appended to color keys as a suffix, rather than a prefix.',
+        ],
+      },
+      notes: [],
+    },
+    {
+      breaking: false,
+      changes: {
+        en: [
+          'Scripts have been rewritten and broken apart for better maintenance, testing, and synchronous error reporting.',
+        ],
+      },
+      notes: [],
+    },
+    {
+      breaking: false,
+      changes: {
+        en: [
+          'The property model has been updated to include new categories to better support existing CSS properties.',
+        ],
+      },
+      notes: [],
+    },
+    {
+      breaking: false,
+      changes: {
+        en: [
+          'Repetitive data has been centralized and standardized to make maintenance and access much easier.',
+        ],
+      },
+      notes: [],
+    },
   ],
   bugfixes: [
+    {
+      breaking: true,
+      changes: {
+        en: [
+          "Hydrogen now presents a targeted error if it can't find your <code>input</code> or <code>output</code> directories, rather than providing unrelated error information,",
+        ],
+      },
+    },
     {
       breaking: true,
       changes: {
@@ -41,6 +117,14 @@ module.exports = {
         ],
       },
       properties: ['offset', 'location'],
+    },
+    {
+      breaking: false,
+      changes: {
+        en: [
+          'Fixes an error that caused the watch script to retain error/warning counts accumulated from previous runs.',
+        ],
+      },
     },
     {
       breaking: false,
