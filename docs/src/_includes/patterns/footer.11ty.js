c867c582dc9e40396a9f806a532e6fb14c76ccd3
Josh Beveridge <git@joshbeveridge.ca>
2023-01-31 12:58:25 -0500

    Theogit add -A!

--- docs/src/_includes/patterns/footer.11ty.js
+++ docs/src/_includes/patterns/footer.11ty.js
@@ -23,12 +23,27 @@ function render(data, props) {
     <footer
       data-h2-display="base(grid)"
       data-h2-grid-template-columns="p-tablet(repeat(2, minmax(0, 1fr)))"
-      data-h2-gap="base(x2)">
-      <div data-h2-text-align="base(center) p-tablet(left)">
+      data-h2-gap="base(x2)"
+      data-h2-padding-bottom="base(x4)"
+      data-h2-overflow="base(hidden)">
+      <div 
+        data-h2-text-align="base(center) p-tablet(left)"
+        data-h2-position="base(relative)">
         <p
           data-h2-font-size="base(h6)"
           data-h2-font-weight="base(700)"
           data-h2-color="base:all(white)">${data.site.name}</p>
+        <div>
+          <img
+            src="/static/img/icon_theo.svg"
+            alt=""
+            data-h2-display="base(inline-block)"
+            data-h2-position="base(absolute)"
+            data-h2-location="p-tablet(auto, auto, -x4, 0)"
+            data-h2-width="base(x3)"
+            data-h2-transition="base(transform .2s ease-in-out)"
+            data-h2-transform="base(translate(0, 60%)) base:hover(translate(0, 0))" />
+        </div>
       </div>
       <div data-h2-text-align="base(center) p-tablet(right)">
         <div
@@ -54,7 +69,7 @@ function render(data, props) {
   `;
   let footer_content = String.raw`
     <div
-      data-h2-padding="base(x4, 0)"
+      data-h2-padding="base(x4, 0, 0, 0)"
       data-h2-position="base(relative)">
       <div ${container}>
         ${footer}
