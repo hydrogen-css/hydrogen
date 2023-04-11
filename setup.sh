396b891389dca78aca4702c55efca83ca791c576
Josh Beveridge <git@joshbeveridge.ca>
2023-04-11 18:41:03 -0400

    Fixes a few outstanding compilation bugs, adds a test environment directory

--- setup.sh
+++ setup.sh
@@ -11,5 +11,12 @@ rm -rf node_modules
 rm -rf package-lock.json
 npm install --no-fund
 npm link @hydrogen-css/hydrogen --no-fund --no-audit
+cd ../test
+rm -rf styles
+mkdir styles
+rm -rf node_modules
+rm -rf package-lock.json
+npm install --no-fund
+npm link @hydrogen-css/hydrogen --no-fund --no-audit
 cd ..
 node lib/scripts/console-logging/log-setup-success/index.js
