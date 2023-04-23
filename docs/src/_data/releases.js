0499334f98d814acb937194c7dfd665541d0061d
Josh Beveridge <git@joshbeveridge.ca>
2023-04-23 09:57:15 -0400

    Organizes the releases folder for better usability, calls releases using glob, consolidates stable and beta functions into releases/index.js

--- docs/src/_data/releases.js
+++ docs/src/_data/releases.js
@@ -1,18 +1,23 @@
+// Hydrogen
 'use strict';
-var exec = require('child_process').execSync;
+
+// Data models
+
+// Data imports
+
+// Local functions
+
+// Helper functions
+
+// Vendor imports
 var path = require('path');
 
-const stable = exec('npm view @hydrogen-css/hydrogen@latest version')
-  .toString()
-  .trim();
-
-const beta = exec('npm view @hydrogen-css/hydrogen@beta version')
-  .toString()
-  .trim();
+// Script ==========================================================================================
 
 module.exports = {
   history: require(path.resolve('../releases')).releases(),
-  latest: require(path.resolve('../releases/' + stable)),
-  beta: require(path.resolve('../releases/' + beta)),
+  latest: require(path.resolve('../releases')).stable(),
+  stable: require(path.resolve('../releases')).stable(),
+  beta: require(path.resolve('../releases')).beta(),
   featured: require(path.resolve('../releases')).featured(),
 };
--- releases/index.js
+++ releases/index.js
@@ -1,30 +1,45 @@
-var fs = require('fs');
+// Hydrogen
+'use strict';
+
+// Data models
+
+// Data imports
+
+// Local functions
+
+// Helper functions
+
+// Vendor imports
+var exec = require('child_process').execSync;
+const { glob } = require('glob');
 var path = require('path');
+
+// External data imports
+const stable_version = exec('npm view @hydrogen-css/hydrogen@latest version').toString().trim();
+const beta_version = exec('npm view @hydrogen-css/hydrogen@beta version').toString().trim();
+
+// Script ==========================================================================================
 
 function releases() {
   try {
     let parsed_data = [];
-    fs.readdirSync(path.resolve(process.cwd(), '../releases')).forEach(
-      (file) => {
-        if (file != 'index.js') {
-          let change_data = require(path.resolve(
-            process.cwd(),
-            '../releases/' + file
-          ));
-          // Get the major, minor, and patch values from the file
-          const version = change_data.version;
-          const version_numbers = version.match(/\d+/g);
-          change_data.major = version_numbers[0];
-          change_data.minor = version_numbers[1];
-          change_data.patch = version_numbers[2];
-          change_data.beta = false;
-          if (version.includes('beta') && version_numbers[3]) {
-            change_data.beta = version_numbers[3];
-          }
-          parsed_data = parsed_data.concat(change_data);
-        }
+    let file_list = glob.sync(path.resolve(process.cwd(), '../releases/**/*.js'), {
+      ignore: [path.resolve(process.cwd(), '../releases/index.js')],
+    });
+    file_list.forEach((file) => {
+      let change_data = require(file);
+      // Get the major, minor, and patch values from the file
+      const version = change_data.version;
+      const version_numbers = version.match(/\d+/g);
+      change_data.major = version_numbers[0];
+      change_data.minor = version_numbers[1];
+      change_data.patch = version_numbers[2];
+      change_data.beta = false;
+      if (version.includes('beta') && version_numbers[3]) {
+        change_data.beta = version_numbers[3];
       }
-    );
+      parsed_data = parsed_data.concat(change_data);
+    });
     let release_data = {};
     // Create major releases
     parsed_data.forEach((release) => {
@@ -62,15 +77,8 @@ function releases() {
       Object.keys(release_data).forEach((major, index) => {
         Object.keys(release_data[major]).forEach((minor, index) => {
           Object.keys(release_data[major][minor]).forEach((patch, index) => {
-            if (
-              release.major === major &&
-              release.minor === minor &&
-              release.patch === patch
-            ) {
-              if (
-                release.beta &&
-                !release_data[major][minor][patch].betas[release.beta]
-              ) {
+            if (release.major === major && release.minor === minor && release.patch === patch) {
+              if (release.beta && !release_data[major][minor][patch].betas[release.beta]) {
                 release_data[major][minor][patch].betas[release.beta] = {};
               }
             }
@@ -84,26 +92,20 @@ function releases() {
         Object.keys(release_data[major]).forEach((minor, index) => {
           Object.keys(release_data[major][minor]).forEach((patch, index) => {
             if (!release.beta) {
-              if (
-                release.major === major &&
-                release.minor === minor &&
-                release.patch === patch
-              ) {
+              if (release.major === major && release.minor === minor && release.patch === patch) {
                 release_data[major][minor][patch].release = release;
               }
             } else {
-              Object.keys(release_data[major][minor][patch].betas).forEach(
-                (beta, index) => {
-                  if (
-                    release.major === major &&
-                    release.minor === minor &&
-                    release.patch === patch &&
-                    release.beta === beta
-                  ) {
-                    release_data[major][minor][patch].betas[beta] = release;
-                  }
+              Object.keys(release_data[major][minor][patch].betas).forEach((beta, index) => {
+                if (
+                  release.major === major &&
+                  release.minor === minor &&
+                  release.patch === patch &&
+                  release.beta === beta
+                ) {
+                  release_data[major][minor][patch].betas[beta] = release;
                 }
-              );
+              });
             }
           });
         });
@@ -118,20 +120,46 @@ function releases() {
 function featured() {
   try {
     let parsed_data = [];
-    fs.readdirSync(path.resolve(process.cwd(), '../releases')).forEach(
-      (file) => {
-        if (file != 'index.js') {
-          let change_data = require(path.resolve(
-            process.cwd(),
-            '../releases/' + file
-          ));
-          if (change_data.featured) {
-            parsed_data.push(change_data);
-          }
-        }
+    let file_list = glob.sync(path.resolve(process.cwd(), '../releases/**/*.js'), {
+      ignore: [path.resolve(process.cwd(), '../releases/index.js')],
+    });
+    file_list.forEach((file) => {
+      let change_data = require(file);
+      if (change_data.featured) {
+        parsed_data.push(change_data);
       }
-    );
+    });
     return parsed_data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function stable() {
+  try {
+    let stable_data = glob.sync(
+      path.resolve(process.cwd(), '../releases/**/' + stable_version + '.js')
+    );
+    if (stable_data) {
+      return require(stable_data[0]);
+    } else {
+      throw new Error('Release data for version ' + stable_version + ' could not be found.');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function beta() {
+  try {
+    let beta_data = glob.sync(
+      path.resolve(process.cwd(), '../releases/**/betas/' + beta_version + '.js')
+    );
+    if (beta_data) {
+      return require(beta_data[0]);
+    } else {
+      throw new Error('Release data for version ' + beta_version + ' could not be found.');
+    }
   } catch (error) {
     console.log(error);
   }
@@ -139,5 +167,7 @@ function featured() {
 
 module.exports = {
   releases,
+  stable,
+  beta,
   featured,
 };
