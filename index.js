b27d75bc8fa6e4a09259a2f375c46f542480c2c3
Josh Beveridge <git@joshbeveridge.ca>
2024-07-04 09:46:27 -0400

    Cleans up code

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 // Hydrogen
+'use strict';
 
-// Data models
-/**
- * @typedef {import('./lib/data/config-data').ParsedConfig} ParsedConfig
- */
-/**
- * @typedef {import('./lib/data/media-array-data').MediaArray} MediaArray
- * @typedef {import('./lib/data/media-array-data').QueryData} QueryData
- */
-
-/**
- * Runs a single instance of Hydrogen's build script
- * @returns {{config: ParsedConfig, media_array: QueryData[], css: string}} an assembled Hydrogen CSS file
- */
 const { hydrogen_build } = require('./lib/build');
-
-/**
- * Runs a Chokidar watcher and builds Hydrogen when changes are detected.
- *
- * @returns {{config: ParsedConfig, media_array: QueryData[], css: string}}
- */
 const { hydrogen_watch } = require('./lib/watch');
 
 module.exports = {
